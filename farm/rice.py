0200f332aa72cb2f3c7dd61d58d3b9fef556d9c1
Aybuke Altuntas <aybukealtuntas@192.168.1.19>
2026-02-13 23:31:34 +0300

    completed rice

--- farm/rice.py
+++ farm/rice.py
@@ -1,5 +1,15 @@
 
 class Rice():
-    # YOUR CODE HERE
-    pass
+    def __init__(self):
+        self.grains = 0
 
+    def water(self):
+        self.grains = self.grains + 5
+        return self.grains
+
+    def ripe(self):
+        return self.grains >= 15
+
+    def transplant(self):
+        self.grains = self.grains + 10
+        return self.grains
